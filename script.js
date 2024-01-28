1decfc89b09def89393e2c552de5adaa2117e62c
Jacob Ye <gmding777@gmail.com>
2024-01-28 13:52:10 -0800

    added color change on div when hovered, removed debugging outline for divs

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ function generateGrid(size) {
             const sizeOfDiv = Math.floor(BOARD_SIZE / size);
             div.style.width = `${sizeOfDiv}px`;
             div.style.height = `${sizeOfDiv}px`;
+            div.addEventListener("mouseover", (e)=>{
+                e.target.style.backgroundColor = "red";
+            });
             containerDiv.appendChild(div);
         }
         mainContainer.appendChild(containerDiv);
@@ -21,4 +24,9 @@ function generateNewGrid() {
     let size = parseInt(prompt("What woud you like the size of the grid to be?", "16"));
     generateGrid(size);
 }
+
+
+document.querySelector("button").addEventListener("click", ()=>{
+    generateNewGrid();
+});
 generateGrid(16);
