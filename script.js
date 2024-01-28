441e84a2085a0472547b0ffe8433a3ecbc9a5631
Jacob Ye <gmding777@gmail.com>
2024-01-28 13:16:40 -0800

    Implemented generating a similar size board while using more divs

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
+const BOARD_SIZE = 900;
+
 function generateGrid(size) {
     const mainContainer = document.querySelector(".container");
+        
     for (let i = 0; i < size; i++) {
         let containerDiv = document.createElement("div");
         for (let j = 0; j < size; j++) {
             let div = document.createElement("div");
+            const sizeOfDiv = Math.floor(BOARD_SIZE / size);
+            div.style.width = `${sizeOfDiv}px`;
+            div.style.height = `${sizeOfDiv}px`;
             containerDiv.appendChild(div);
         }
         mainContainer.appendChild(containerDiv);
     }
 }
+
 
 generateGrid(16);
